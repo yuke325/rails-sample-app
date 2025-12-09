39378727128c8db05e2d1bfd15baee3997bb157c
yuke325 <yusukesaikyou931@gmail.com>
2025-12-10 02:28:43 +0900

    test: root hello -> home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  root 'static_pages#home'
 
   # Defines the root path route ("/")
   # root "articles#index"
